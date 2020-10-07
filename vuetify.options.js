c8d7d615f2fed8aaf27bcacc52d18e5ca16058d3
kimamula <kenji.imamula@gmail.com>
2020-10-07 20:38:45 +0900

    first commit

---
+++ vuetify.options.js
@@ -0,0 +1,18 @@
+import colors from "vuetify/es5/util/colors";
+
+export default {
+  theme: {
+    dark: false,
+    themes: {
+      light: {
+        primary: '#009688',
+        secondary: '#ff5722',
+        accent: '#e91e63',
+        error: '#f44336',
+        warning: '#ff9800',
+        info: '#607d8b',
+        success: '#4caf50'
+      }
+    }
+  }
+};
